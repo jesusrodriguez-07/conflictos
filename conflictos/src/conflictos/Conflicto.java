d2cfce54f47b4b6c4b11ffe48a235b42c097c426
danielmunoz-sketch <daniel.munoz@iesnervion.es>
2026-01-12 12:28:50 +0100

    conflictos

---
+++ conflictos/src/conflictos/Conflicto.java
@@ -0,0 +1,16 @@
+package conflictos;
+
+public class Conflicto {
+
+	public static void main(String[] args) {
+		
+		int x=1;
+		
+		System.out.println("Hola, soy Daniel Muñoz");
+
+		do {
+			System.out.println("Esto es un bucle");
+		}while (x==0);
+	}
+
+}
